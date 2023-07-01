af0e21e332e898f5cb606cacb462fea0ec7b0520
Shubham Goyal <goyalshubham2521@gmail.com>
2023-07-02 00:47:46 +0530

    Create:allcomponents

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { useState } from "react";
 
-function App() {
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion data={faqs} />
     </div>
   );
 }
 
-export default App;
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((item, i) => (
+        <AccordionItem num={i} title={item.title} text={item.text} key={i} />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, text, title }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle(e) {
+    setIsOpen((isOpen) => !isOpen);
+  }
+  return (
+    <div
+      className={isOpen === true ? "item open" : "item"}
+      onClick={handleToggle}
+    >
+      <p className="number">{num < 9 ? `0${num + 1}` : num}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen === false ? "+" : "-"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
+    </div>
+  );
+}
